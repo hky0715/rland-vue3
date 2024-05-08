2b0321f63c2c941e2f3197c8ff317d9643753c4b
hky0715 <hky0715@gmail.com>
2024-05-08 14:45:22 +0900

    icon import through static directory, vue project build using 'npm run dev'

--- src/components/inc/Header.vue
+++ src/components/inc/Header.vue
@@ -10,8 +10,8 @@
             <h1 class="d:none">메인메뉴</h1>
             <ul>
                 <!-- <li><a href=""></a></li> -->
-                <li><RouterLink class="icon icon:house md:d:none mr:1" to="/index">home</RouterLink></li>
-                <li><RouterLink class="icon icon:list d:none md:d:inline-block mr:1" to="/menu/list">메뉴</RouterLink></li>
+                <li><RouterLink class="icon icon:house d:none md:d:inline-block mr:1" to="/index">home</RouterLink></li>
+                <li><RouterLink class="icon icon:list md:d:none mr:1" to="/menu/list">메뉴</RouterLink></li>
                 <li><RouterLink class="icon icon:local-cafe d:none md:d:inline-block mr:1" to="/menu/list">메뉴</RouterLink></li>
                 <li><RouterLink class="icon icon:user d:none md:d:inline-block mr:1" to="/admin">관리자</RouterLink></li>
             </ul>
